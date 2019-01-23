97696c8ae4f62d0bca22941963cae1078d9fe67f
man  hua <zhangfby@163.com>
2019-01-23 04:01:42 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="http://www.bygogo.com" target="_blank">
+    刻意练习，每日精进
+  </a>
+
+  <div class="float-right">
+    <a href="/about"> 关于 </a>
+  </div>
+</footer>

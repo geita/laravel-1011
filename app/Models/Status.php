3bb3a64f320ce41aafd0eb8f4af5fa2e231e2925
man  hua <zhangfby@163.com>
2019-01-29 07:05:13 +0000

    用户微博列表

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    public fucntion user()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
